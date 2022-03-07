a4f3a9dc27bdc10f90d3116fe31b6579d886b237
Walker O'Brien <walkernobrien@gmail.com>
2022-03-06 23:09:05 -0600

    ugh

--- cicd.tf
+++ cicd.tf
@@ -63,20 +63,36 @@ resource "google_cloudbuild_trigger" "cloudbuild_trigger" {
     }
 
     step {
-      id         = "GKE-Auth"
-      name       = "gcr.io/cloud-builders/gcloud-slim"
+      id = "GKE-Auth"
+      name = "gcr.io/cloud-builders/gcloud-slim"
       entrypoint = "bash"
       args = [
-        "-c", "gcloud container clusters get-credentials ${var.cluster_name} --zone=${var.zone} --project=${var.gke_project_id}",
+        "-c",
+        "gcloud", "container", "clusters", "get-credentials",
+        "${var.cluster_name}", "--zone=${var.zone}", "--project=${var.gke_project_id}",
       ]
     }
 
     step {
-      id         = "Helm-Upgrade"
-      name       = "gcr.io/walker-cpl/helm"
+      id = "Helm-Upgrade"
+      name = "gcr.io/walker-cpl/helm"
       entrypoint = "bash"
       args = [
-        "-c", "helm dep update ./charts/web-app && helm upgrade -i ${var.lifecycle_name}-${var.repo_name} ./charts/web-app -f ./charts/web-app/values/${var.lifecycle_name}.yaml -n ${var.lifecycle_name}-${var.repo_name} --set web-app.api.tag=$COMMIT_SHA --set web-app.nginx.tag=$COMMIT_SHA --set web-app.app_project_id=${var.app_project_id} --set web-app.gke_project_id=${var.gke_project_id} --set web-app.db_project_id=${var.db_project_id} --set web-app.google.domain=${var.domain} --set web-app.app_code=${var.repo_name}",
+        "-c",
+        "helm", "dep", "update",
+        "./charts/web-app",
+        "&&",
+        "helm", "upgrade", "-i", "${var.lifecycle_name}-${var.repo_name}",
+        "./charts/web-app",
+        "-f", "./charts/web-app/values/${var.lifecycle_name}.yaml",
+        "-n", "${var.lifecycle_name}-${var.repo_name}",
+        "--set", "web-app.api.tag=$COMMIT_SHA",
+        "--set", "web-app.nginx.tag=$COMMIT_SHA",
+        "--set", "web-app.app_project_id=${var.app_project_id}",
+        "--set", "web-app.gke_project_id=${var.gke_project_id}",
+        "--set", "web-app.db_project_id=${var.db_project_id}",
+        "--set", "web-app.google.domain=${var.domain}",
+        "--set", "web-app.app_code=${var.repo_name}",
       ]
     }
 
