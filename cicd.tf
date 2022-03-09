20c43cc33aa70842c6bd9c40e275070dcc7e89b4
Walker O'Brien <walkernobrien@gmail.com>
2022-03-09 01:28:04 -0600

    until we fix config connect

--- cicd.tf
+++ cicd.tf
@@ -6,7 +6,7 @@ resource "google_cloudbuild_trigger" "cloudbuild_trigger_legacy" {
 
   disabled       = var.disabled
   included_files = var.included_files
-  service_account = local.cicd_service_account
+  # service_account = local.cicd_service_account
 
   github {
     owner = var.github_owner
