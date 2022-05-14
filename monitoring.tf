1be224e58455198ebeadac85efbea2e8940528e5
Walker O'Brien <walkernobrien@gmail.com>
2022-05-14 10:22:02 -0500

    update

--- monitoring.tf
+++ monitoring.tf
@@ -1,147 +1,147 @@
 
-resource "google_monitoring_dashboard" "dashboard" {
-  count = !var.disabled ? 1 : 0
-
-  project = var.gke_project_id
-
-  dashboard_json = <<EOF
-{
-  "category": "CUSTOM",
-  "displayName": "${local.app_label}",
-  "mosaicLayout": {
-    "columns": 12,
-    "tiles": [
-      {
-        "height": 4,
-        "widget": {
-          "title": "Memory Usage",
-          "xyChart": {
-            "chartOptions": {
-              "mode": "COLOR"
-            },
-            "dataSets": [
-              {
-                "minAlignmentPeriod": "60s",
-                "plotType": "LINE",
-                "targetAxis": "Y1",
-                "timeSeriesQuery": {
-                  "apiSource": "DEFAULT_CLOUD",
-                  "timeSeriesFilter": {
-                    "aggregation": {
-                      "alignmentPeriod": "60s",
-                      "crossSeriesReducer": "REDUCE_NONE",
-                      "perSeriesAligner": "ALIGN_MEAN"
-                    },
-                    "filter": "metric.type=\"kubernetes.io/container/memory/used_bytes\" resource.type=\"k8s_container\" resource.label.\"namespace_name\"=\"${local.app_label}\""
-                  }
-                }
-              }
-            ],
-            "timeshiftDuration": "0s",
-            "yAxis": {
-              "label": "y1Axis",
-              "scale": "LINEAR"
-            }
-          }
-        },
-        "width": 6,
-        "xPos": 6,
-        "yPos": 0
-      },
-      {
-        "height": 4,
-        "widget": {
-          "title": "Persistent Disk Usage",
-          "xyChart": {
-            "chartOptions": {
-              "mode": "COLOR"
-            },
-            "dataSets": [
-              {
-                "minAlignmentPeriod": "60s",
-                "plotType": "LINE",
-                "targetAxis": "Y1",
-                "timeSeriesQuery": {
-                  "apiSource": "DEFAULT_CLOUD",
-                  "timeSeriesFilter": {
-                    "aggregation": {
-                      "alignmentPeriod": "60s",
-                      "crossSeriesReducer": "REDUCE_NONE",
-                      "perSeriesAligner": "ALIGN_MEAN"
-                    },
-                    "filter": "metric.type=\"kubernetes.io/pod/volume/used_bytes\" resource.type=\"k8s_pod\" resource.label.\"namespace_name\"=\"${local.app_label}\" metric.label.\"volume_name\"=\"gce-volume\""
-                  }
-                }
-              }
-            ],
-            "timeshiftDuration": "0s",
-            "yAxis": {
-              "label": "y1Axis",
-              "scale": "LINEAR"
-            }
-          }
-        },
-        "width": 6,
-        "xPos": 0,
-        "yPos": 0
-      },
-      {
-        "height": 4,
-        "widget": {
-          "title": "CPU Usage",
-          "xyChart": {
-            "chartOptions": {
-              "mode": "COLOR"
-            },
-            "dataSets": [
-              {
-                "minAlignmentPeriod": "60s",
-                "plotType": "LINE",
-                "targetAxis": "Y1",
-                "timeSeriesQuery": {
-                  "apiSource": "DEFAULT_CLOUD",
-                  "timeSeriesFilter": {
-                    "aggregation": {
-                      "alignmentPeriod": "60s",
-                      "crossSeriesReducer": "REDUCE_NONE",
-                      "perSeriesAligner": "ALIGN_RATE"
-                    },
-                    "filter": "metric.type=\"kubernetes.io/container/cpu/core_usage_time\" resource.type=\"k8s_container\" resource.label.\"namespace_name\"=\"${local.app_label}\"",
-                    "secondaryAggregation": {
-                      "alignmentPeriod": "60s",
-                      "crossSeriesReducer": "REDUCE_NONE",
-                      "perSeriesAligner": "ALIGN_MEAN"
-                    }
-                  }
-                }
-              }
-            ],
-            "timeshiftDuration": "0s",
-            "yAxis": {
-              "label": "y1Axis",
-              "scale": "LINEAR"
-            }
-          }
-        },
-        "width": 6,
-        "xPos": 0,
-        "yPos": 4
-      },
-      {
-        "height": 4,
-        "widget": {
-          "alertChart": {
-            "name": "projects/${var.gke_project_id}/alertPolicies/7453247611856152035"
-          }
-        },
-        "width": 6,
-        "xPos": 6,
-        "yPos": 4
-      }
-    ]
-  }
-}
-EOF
-}
+# resource "google_monitoring_dashboard" "dashboard" {
+#   count = !var.disabled ? 1 : 0
+#
+#   project = var.gke_project_id
+#
+#   dashboard_json = <<EOF
+# {
+#   "category": "CUSTOM",
+#   "displayName": "${local.app_label}",
+#   "mosaicLayout": {
+#     "columns": 12,
+#     "tiles": [
+#       {
+#         "height": 4,
+#         "widget": {
+#           "title": "Memory Usage",
+#           "xyChart": {
+#             "chartOptions": {
+#               "mode": "COLOR"
+#             },
+#             "dataSets": [
+#               {
+#                 "minAlignmentPeriod": "60s",
+#                 "plotType": "LINE",
+#                 "targetAxis": "Y1",
+#                 "timeSeriesQuery": {
+#                   "apiSource": "DEFAULT_CLOUD",
+#                   "timeSeriesFilter": {
+#                     "aggregation": {
+#                       "alignmentPeriod": "60s",
+#                       "crossSeriesReducer": "REDUCE_NONE",
+#                       "perSeriesAligner": "ALIGN_MEAN"
+#                     },
+#                     "filter": "metric.type=\"kubernetes.io/container/memory/used_bytes\" resource.type=\"k8s_container\" resource.label.\"namespace_name\"=\"${local.app_label}\""
+#                   }
+#                 }
+#               }
+#             ],
+#             "timeshiftDuration": "0s",
+#             "yAxis": {
+#               "label": "y1Axis",
+#               "scale": "LINEAR"
+#             }
+#           }
+#         },
+#         "width": 6,
+#         "xPos": 6,
+#         "yPos": 0
+#       },
+#       {
+#         "height": 4,
+#         "widget": {
+#           "title": "Persistent Disk Usage",
+#           "xyChart": {
+#             "chartOptions": {
+#               "mode": "COLOR"
+#             },
+#             "dataSets": [
+#               {
+#                 "minAlignmentPeriod": "60s",
+#                 "plotType": "LINE",
+#                 "targetAxis": "Y1",
+#                 "timeSeriesQuery": {
+#                   "apiSource": "DEFAULT_CLOUD",
+#                   "timeSeriesFilter": {
+#                     "aggregation": {
+#                       "alignmentPeriod": "60s",
+#                       "crossSeriesReducer": "REDUCE_NONE",
+#                       "perSeriesAligner": "ALIGN_MEAN"
+#                     },
+#                     "filter": "metric.type=\"kubernetes.io/pod/volume/used_bytes\" resource.type=\"k8s_pod\" resource.label.\"namespace_name\"=\"${local.app_label}\" metric.label.\"volume_name\"=\"gce-volume\""
+#                   }
+#                 }
+#               }
+#             ],
+#             "timeshiftDuration": "0s",
+#             "yAxis": {
+#               "label": "y1Axis",
+#               "scale": "LINEAR"
+#             }
+#           }
+#         },
+#         "width": 6,
+#         "xPos": 0,
+#         "yPos": 0
+#       },
+#       {
+#         "height": 4,
+#         "widget": {
+#           "title": "CPU Usage",
+#           "xyChart": {
+#             "chartOptions": {
+#               "mode": "COLOR"
+#             },
+#             "dataSets": [
+#               {
+#                 "minAlignmentPeriod": "60s",
+#                 "plotType": "LINE",
+#                 "targetAxis": "Y1",
+#                 "timeSeriesQuery": {
+#                   "apiSource": "DEFAULT_CLOUD",
+#                   "timeSeriesFilter": {
+#                     "aggregation": {
+#                       "alignmentPeriod": "60s",
+#                       "crossSeriesReducer": "REDUCE_NONE",
+#                       "perSeriesAligner": "ALIGN_RATE"
+#                     },
+#                     "filter": "metric.type=\"kubernetes.io/container/cpu/core_usage_time\" resource.type=\"k8s_container\" resource.label.\"namespace_name\"=\"${local.app_label}\"",
+#                     "secondaryAggregation": {
+#                       "alignmentPeriod": "60s",
+#                       "crossSeriesReducer": "REDUCE_NONE",
+#                       "perSeriesAligner": "ALIGN_MEAN"
+#                     }
+#                   }
+#                 }
+#               }
+#             ],
+#             "timeshiftDuration": "0s",
+#             "yAxis": {
+#               "label": "y1Axis",
+#               "scale": "LINEAR"
+#             }
+#           }
+#         },
+#         "width": 6,
+#         "xPos": 0,
+#         "yPos": 4
+#       },
+#       {
+#         "height": 4,
+#         "widget": {
+#           "alertChart": {
+#             "name": "projects/${var.gke_project_id}/alertPolicies/7453247611856152035"
+#           }
+#         },
+#         "width": 6,
+#         "xPos": 6,
+#         "yPos": 4
+#       }
+#     ]
+#   }
+# }
+# EOF
+# }
 
 
