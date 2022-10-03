107787c49aff3a029251a8d652810fd4d0e880df
Walker O'Brien <walkernobrien@gmail.com>
2022-10-02 21:41:53 -0500

    new chart

--- variables.tf
+++ variables.tf
@@ -394,6 +394,6 @@ variable "chart_values_path" {
 variable "chart_version" {
   description = "Application helm chart version"
   type        = string
-  default     = "0.3.46"
+  default     = "0.3.47"
 }
 
