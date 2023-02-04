a96c471f470124ea80725021c96aab814688d7e4
Walker O'Brien <walkernobrien@gmail.com>
2023-02-04 16:44:34 -0600

    fix type checking

--- key.tf
+++ key.tf
@@ -1,16 +1,17 @@
 
 resource "time_rotating" "key_rotation" {
+  count = !var.disabled && var.use_django ? 1 : 0
   rotation_days = 30
 }
 
 
 resource "google_service_account_key" "key" {
-  count = !var.disabled && var.use_django != "" ? 1 : 0
+  count = !var.disabled && var.use_django ? 1 : 0
 
   service_account_id = var.service_account
 
   keepers = {
-    rotation_time = time_rotating.key_rotation.rotation_rfc3339
+    rotation_time = time_rotating.key_rotation[0].rotation_rfc3339
   }
 }
 
